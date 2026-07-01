6af85b41b3d3484ea77af9d529934374a7ccab85
ashpakabp <pathanabp77@gmail.com>
2026-07-01 16:45:53 +0530

    Update CarController.java

--- CarController.java
+++ CarController.java
@@ -3,7 +3,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import com.carsales.entity.Car;
 import com.carsales.service.CarService;
 
