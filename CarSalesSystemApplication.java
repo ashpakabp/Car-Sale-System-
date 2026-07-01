976c5c2779a8140dd6e32e0db916e5f91f740710
ashpakabp <pathanabp77@gmail.com>
2026-07-01 16:46:20 +0530

    Update CarSalesSystemApplication.java

--- CarSalesSystemApplication.java
+++ CarSalesSystemApplication.java
@@ -1,7 +1,6 @@
 package com.carsales;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CarSalesSystemApplication {
     public static void main(String[] args) {
