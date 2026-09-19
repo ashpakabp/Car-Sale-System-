39f1b9ae7242ede37b840d53048f729f61e5b722
ashpakabp <pathanabp77@gmail.com>
2026-09-19 23:39:56 +0530

    Update CarRepository.java

--- CarRepository.java
+++ CarRepository.java
@@ -1,4 +1,4 @@
-package com.carsales.repository;
+abcd package com.carsales.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.carsales.entity.Car;
 public interface CarRepository extends JpaRepository<Car, Long> {
